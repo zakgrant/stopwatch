f9794ad25e62a410608d873c316b3965b63c620a
Zak Grant <zakgrant@gmail.com>
2012-10-24 23:50:12 +0100

    initial completion

--- stopwatch/AppDelegate.rb
+++ stopwatch/AppDelegate.rb
@@ -8,8 +8,35 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :textField # This creates an outlet named textField
+
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+    end
+    
+    def startTimer(sender)
+        if @timer.nil?
+            @time = 0.0
+            @timer = NSTimer
+            .scheduledTimerWithTimeInterval(0.1,
+                                            target: self,
+                                            selector: "timerHandler:",
+                                            userInfo: nil,
+                                            repeats: true)
+        end
+    end
+    
+    def stopTimer(sender)
+        if @timer
+            @timer.invalidate
+            @timer = nil
+        end
+    end
+    
+    def timerHandler(userInfo)
+        @time += 0.1
+        string = sprintf("%.1f", @time)
+        textField.setStringValue(string)
     end
 end
 
